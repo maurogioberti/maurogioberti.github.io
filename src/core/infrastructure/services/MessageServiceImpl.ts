1477f425b080073bc587c61ff6a4660ca288023d
Mauro Gioberti <maurol.gioberti@gmail.com>
2024-11-27 16:38:48 +0100

    🧪 test dummy commit (remove this)

--- src/core/infrastructure/services/MessageServiceImpl.ts
+++ src/core/infrastructure/services/MessageServiceImpl.ts
@@ -1,24 +1,13 @@
 import { BaseService } from "./base/BaseService";
 import { MessageService } from "../../domain/services/MessageService";
 import { Message } from "../../domain/model/Message";
-import fs from "fs/promises";
-import path from "path";
 
 export class MessageServiceImpl extends BaseService implements MessageService {
-  private static readonly MESSAGE_FILE_PATH = path.resolve(
-    process.cwd(),
-    "src/data/message.json"
-  ); //TODO: Move base path to base service class
+  private static readonly MESSAGE_API_URL: string = "https://run.mocky.io/v3/c1c6aa61-e952-4f96-bc2a-5ab1cf97f4a7";
 
-  //TODO: Move to base class
   async fetchMessage(): Promise<Message> {
-    try {
-      const fileContent = await fs.readFile(MessageServiceImpl.MESSAGE_FILE_PATH, "utf-8");
-      const data: Message = JSON.parse(fileContent); 
-      return data;
-    } catch (error) {
-      console.error("Error reading message.json:", error);
-      throw new Error("Could not read message.json");
-    }
+    const response = await fetch(MessageServiceImpl.MESSAGE_API_URL);
+    const data: Message = await response.json();
+    return data;
   }
 }

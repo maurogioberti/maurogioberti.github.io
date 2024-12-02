3c9560321ccfd98001d5fc904092fb36e56cccd5
Mauro Gioberti <maurol.gioberti@gmail.com>
2024-12-02 04:02:30 +0100

    👁️ Fix Header Navbar Overlap

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,12 +5,12 @@ export async function Header() {
   const { content, profile } = await headerViewModel();
 
   return (
-    <header className="bg-vs-background-light text-vs-foreground shadow-md fixed w-full top-0">
-      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-16">
+    <header className="bg-vs-background-light text-vs-foreground shadow-md w-full h-16">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center h-full">
         <div className="text-lg font-semibold">
-            <Link href="/" className="hover:text-vs-primary">
-              {profile?.fullname}
-            </Link>
+          <Link href="/" className="hover:text-vs-primary">
+            {profile?.fullname}
+          </Link>
         </div>
         <ul className="flex space-x-4">
           {content.map((link: { href: string; label: string }) => (
